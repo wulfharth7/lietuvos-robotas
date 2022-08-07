3b2fe79ca34469ab8c16f89d13d1b25b32c862f1
wulfharth7 <legendcrafter24@gmail.com>
2022-08-07 09:25:41 +0300

    the bot can differ words from each other and decide if they are noun or verb, then create the table accourding to that, im planning to add more functionalities for that and also consider other languages rather than lithuanian, such as german or french

--- src/util/extract-word.js
+++ src/util/extract-word.js
@@ -1,6 +1,6 @@
 const extractor = function(query){
     const pattern = /<[a-zA-Z]+>/; //only works with latin letters for now, to be updated
-    const stringToSplit = query;
+    const stringToSplit = query; //make it case insensitive
     const extractedWord= stringToSplit.match(pattern)
     extractedWord[0] = extractedWord[0].replace("<","")
     extractedWord[0] = extractedWord[0].replace(">","")
--- src/util/pretty-stringified.js
+++ src/util/pretty-stringified.js
@@ -1,6 +1,22 @@
 const tablemark = require('tablemark')
 
 const prettifier = function(query){
+    const decider = nounOrVerb(query);
+    if(decider == "noun"){
+        return declineNouns(query)
+    }else if(decider == "verb"){
+        return conjugateVerbs(query)
+    }
+}
+
+const convertArrays = function(array){
+    var newArr = [];
+    for(var i = 0; i < array.length; i++){
+    newArr = newArr.concat(array[i]);
+    }
+    return newArr
+}
+const declineNouns = function (query){
     const newArr = convertArrays(query)
     var tidiedArr = []
     for (var i = 0; i < newArr.length; i++) {
@@ -18,15 +34,33 @@ const prettifier = function(query){
     ])
     return redditTable;
 }
-
-const convertArrays = function(array){
-    var newArr = [];
-    for(var i = 0; i < array.length; i++){
-    newArr = newArr.concat(array[i]);
+const conjugateVerbs = function(query){
+    const newArr = convertArrays(query)
+    var tidiedArr = []
+    for (var i = 0; i < newArr.length; i++) {
+        tidiedArr.push(newArr[i]["Jie/jos"])
+        tidiedArr.push(newArr[i]["Esamasis laikas"])
+        tidiedArr.push(newArr[i]["Būtasis kartinis laikas"])
+        tidiedArr.push(newArr[i]["Būtasis dažninis"])
     }
-    return newArr
+    var redditTable = tablemark([
+        {Įvardis:"Aš","Esamasis laikas":tidiedArr[0],"Būtasis kartinis laikas":tidiedArr[1],"Būtasis dažninis":tidiedArr[2],"Būsimasis laikas":tidiedArr[3]},
+        {Įvardis:"Tu","Esamasis laikas":tidiedArr[4],"Būtasis kartinis laikas":tidiedArr[5],"Būtasis dažninis":tidiedArr[6],"Būsimasis laikas":tidiedArr[7]},
+        {Įvardis:"Jis/ji","Esamasis laikas":tidiedArr[8],"Būtasis kartinis laikas":tidiedArr[9],"Būtasis dažninis":tidiedArr[10],"Būsimasis laikas":tidiedArr[11]},
+        {Įvardis:"Mes","Esamasis laikas":tidiedArr[12],"Būtasis kartinis laikas":tidiedArr[13],"Būtasis dažninis":tidiedArr[14],"Būsimasis laikas":tidiedArr[15]},
+        {Įvardis:"Jūs","Esamasis laikas":tidiedArr[16],"Būtasis kartinis laikas":tidiedArr[17],"Būtasis dažninis":tidiedArr[18],"Būsimasis laikas":tidiedArr[19]},
+        {Įvardis:"Jie/jos","Esamasis laikas":tidiedArr[20],"Būtasis kartinis laikas":tidiedArr[21],"Būtasis dažninis":tidiedArr[22],"Būsimasis laikas":tidiedArr[23]},
+    ])
+    return redditTable;
 }
-
+const nounOrVerb = function(array){
+   const listOfTable = Object.keys(array[0][0]);
+   if(listOfTable[0] == "Jie/jos"){
+        return "verb"
+   }else if(listOfTable[0] == "Š."){
+        return "noun"
+   }
+}
 module.exports = {
     prettifier:prettifier
 }
